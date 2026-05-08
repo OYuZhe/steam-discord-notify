e1e88cff6f35d2937fed2a730c786041b9c0dfb4
OYuZhe <49910190+OYuZhe@users.noreply.github.com>
2026-05-08 11:55:55 +0800

    Update notify.php

--- notify.php
+++ notify.php
@@ -1,1 +1,28 @@
+<?php
 
+$webhook = getenv('DISCORD_WEBHOOK');
+
+if (!$webhook) {
+    exit('Webhook not found');
+}
+
+$data = [
+    'content' => 'GitHub Actions 測試成功'
+];
+
+$ch = curl_init($webhook);
+
+curl_setopt_array($ch, [
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => json_encode($data),
+    CURLOPT_HTTPHEADER => [
+        'Content-Type: application/json'
+    ],
+    CURLOPT_RETURNTRANSFER => true
+]);
+
+$result = curl_exec($ch);
+
+curl_close($ch);
+
+echo $result;
